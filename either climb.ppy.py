967c4a333e307b986895a6747e171bc079d1cd0c
hemanihemu <113302663+hemanihemu@users.noreply.github.com>
2022-09-12 14:39:16 +0530

    Add files via upload

---
+++ either climb.ppy.py
@@ -0,0 +1,24 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/medar/AppData/Local/Programs/Python/Python310/either climb.py
+n=2
+2
+>>> 
+==================================================== RESTART: C:/Users/medar/AppData/Local/Programs/Python/Python310/either climb.py ===================================================
+n=3
+3
+>>> 
+==================================================== RESTART: C:/Users/medar/AppData/Local/Programs/Python/Python310/either climb.py ===================================================
+n=4
+5
+>>> 
+==================================================== RESTART: C:/Users/medar/AppData/Local/Programs/Python/Python310/either climb.py ===================================================
+n=1
+1
+>>> 
+==================================================== RESTART: C:/Users/medar/AppData/Local/Programs/Python/Python310/either climb.py ===================================================
+n=5
+8
+>>> 
+==================================================== RESTART: C:/Users/medar/AppData/Local/Programs/Python/Python310/either climb.py ===================================================
