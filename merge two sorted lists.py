4bca25e063d8656de8ef97f87eb844692e460479
hemanihemu <113302663+hemanihemu@users.noreply.github.com>
2022-09-13 14:02:48 +0530

    Create merge two sorted lists.py

---
+++ merge two sorted lists.py
@@ -0,0 +1,7 @@
+test_list1=[]
+test_list2=[1,2,3,4,5]
+print("the list 1 is:",test_list1)
+print("the list 2 is:",test_list2)
+test_list1.extend(test_list2)
+test_list1.sort()
+print("the combined sortedlist is:",test_list1)
